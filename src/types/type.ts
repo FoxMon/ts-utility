b14db867b6b1247e4c12cf7900e4a0b926e4aeea
foxmon <jjockrod@naver.com>
2023-02-17 21:50:45 +0900

    array

---
+++ src/types/type.ts
@@ -0,0 +1,13 @@
+/**
+ * Types
+ */
+export namespace Type {
+  /**
+   * Create match context
+   * @returns {on, otherwise} function
+   */
+  export interface MatchType<T, V> {
+    on: (pred: (v: T) => boolean, fn: (v: T) => V) => MatchType<T, V>;
+    otherwise: (fn: (x: T) => V) => V;
+  }
+}
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,7 +1,20 @@
+import { Type } from "../types/type";
+
+/**
+ * Remove duplicate
+ * @param arr1
+ * @param arr2
+ * @returns {Array}
+ */
 export function removeDuplicate<T>(arr1: Array<T>, arr2: Array<T>): Array<T> {
   return arr1.filter((value: T, idx: number) => arr2.indexOf(value) === idx);
 }
 
+/**
+ * Async Array.forEach
+ * @param arr
+ * @param cb callback
+ */
 export async function asyncEach<T>(
   arr: Array<T>,
   cb: (item: T) => Promise<any>
@@ -12,4 +25,29 @@ export async function asyncEach<T>(
   for (const index of indexes) {
     await cb(arr[index]);
   }
+}
+
+/**
+ * Create matched context
+ * @param v
+ * @returns {on, otherwise}
+ */
+export function matched<T>(v: T) {
+  return {
+    on: () => matched(v),
+    otherwise: () => v,
+  };
+}
+
+/**
+ * Create match context
+ * @param v
+ * @returns {on, otherwise}
+ */
+export function match<T, V>(v: T): Type.MatchType<T, V> {
+  return {
+    on: (predict: (v: T) => boolean, fn: (v: T) => V) =>
+      predict(v) ? matched(fn(v)) : match(v),
+    otherwise: (fn: (x: T) => V) => fn(v),
+  };
 }
