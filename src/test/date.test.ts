c368cddb4b057652e46f495f79b112556e86c03f
foxmon <jjockrod@naver.com>
2023-02-22 21:18:01 +0900

    date

--- src/test/date.test.ts
+++ src/test/date.test.ts
@@ -1,5 +1,12 @@
 import { describe, it, expect } from "vitest";
-import { addDate, addMonth, addYear } from "../utils/date";
+import {
+  addDate,
+  addMonth,
+  addYear,
+  yearDiff,
+  monthDiff,
+  dateDiff,
+} from "../utils/date";
 
 describe("Date util test", () => {
   it("Add year function test", () => {
@@ -7,16 +14,45 @@ describe("Date util test", () => {
     const res = addYear(now, 3);
     expect(res.getFullYear()).toEqual(now.getFullYear() + 3);
   });
+
   it("Add month function test", () => {
     const now = new Date();
     const res = addMonth(now, 1);
     expect(now.getMonth() + 1).toEqual(res.getMonth());
   });
+
   it("Add date function test", () => {
     const now = new Date();
     const res = addDate(now, 13);
     const addedNow = new Date(now.getTime());
     addedNow.setDate(addedNow.getDate() + 13);
     expect(addedNow.getDate()).toEqual(res.getDate());
+  });
+
+  it("Diff year function test", () => {
+    const now = new Date();
+    const next = addYear(new Date(), 3);
+    const diff = yearDiff(now, next);
+    const diff2 = yearDiff(next, now);
+    expect(diff).toBe(3);
+    expect(diff2).toBe(3);
+  });
+
+  it("Diff month function test", () => {
+    const now = new Date();
+    const next = addMonth(new Date(), 3);
+    const diff = monthDiff(now, next);
+    const diff2 = monthDiff(next, now);
+    expect(diff).toBe(3);
+    expect(diff2).toBe(3);
+  });
+
+  it("Diff date function test", () => {
+    const now = new Date();
+    const next = addDate(new Date(), 3);
+    const diff = dateDiff(now, next);
+    const diff2 = dateDiff(next, now);
+    expect(diff).toBe(3);
+    expect(diff2).toBe(3);
   });
 });
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -37,3 +37,42 @@ export function addDate<T extends Date | null>(target: T, value: number): T {
   added.setDate(added.getDate() + value);
   return added as T;
 }
+
+/**
+ * Year diff function
+ * @param {Date} target1
+ * @param {Date} target2
+ * @returns {number | null}
+ */
+export function yearDiff(target1: Date, target2: Date): number | null {
+  if (!target1 || !target2) return null;
+  const t1Year: number = target1.getFullYear();
+  const t2Year: number = target2.getFullYear();
+  return t1Year >= t2Year ? t1Year - t2Year : t2Year - t1Year;
+}
+
+/**
+ * Month diff function
+ * @param {Date} target1
+ * @param {Date} target2
+ * @returns {number | null}
+ */
+export function monthDiff(target1: Date, target2: Date): number | null {
+  if (!target1 || !target2) return null;
+  const t1Month: number = target1.getMonth();
+  const t2Month: number = target2.getMonth();
+  return t1Month >= t2Month ? t1Month - t2Month : t2Month - t1Month;
+}
+
+/**
+ * Date diff function
+ * @param {Date} target1
+ * @param {Date} target2
+ * @returns {number | null}
+ */
+export function dateDiff(target1: Date, target2: Date): number | null {
+  if (!target1 || !target2) return null;
+  const t1Date: number = target1.getDate();
+  const t2Date: number = target2.getDate();
+  return t1Date >= t2Date ? t1Date - t2Date : t2Date - t1Date;
+}
