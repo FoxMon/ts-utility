2fb171f87683beaddd56247a4995a98fce6076e8
foxmon <jjockrod@naver.com>
2023-03-04 00:19:45 +0900

    fix

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as array from "./utils/array";
+import * as assert from "./utils/assert";
+import * as date from "./utils/date";
+import * as is from "./utils/is";
+import * as object from "./utils/object";
+
+export { array, assert, date, is, object };
--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import * as array from "./array";
-import * as assert from "./assert";
-import * as date from "./date";
-import * as is from "./is";
-import * as object from "./object";
-
-export { array, assert, date, is, object };
