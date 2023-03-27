eb0b76dc6ad3d98ac5cf3163dcf89bd2ca825fb2
foxmon <jjockrod@naver.com>
2023-03-27 21:32:36 +0900

    function

--- src/utils/func.ts
+++ src/utils/func.ts
@@ -32,3 +32,48 @@ export function match<T, V>(v: T): Type.MatchType<T, V> {
     otherwise: (fn: (x: T) => V) => fn(v),
   };
 }
+
+/**
+ * Size of array or object
+ * @param {any} value
+ * @returns {number}
+ */
+export function size(value: any): number {
+  return Array.isArray(value)
+    ? value.length
+    : value && typeof value === "object"
+    ? value.size || value.length || Object.keys(value).length
+    : typeof value === "string"
+    ? new Blob([value]).size
+    : 0;
+}
+
+/**
+ * Throttle
+ * @param {Function} func
+ * @param {number} wait
+ * @returns {Function}
+ */
+export function throttle(func: Function, wait: number = 250) {
+  let isThrottle: boolean;
+  let lastFunc: ReturnType<typeof setTimeout>;
+  let lastTime: number;
+  return function (this: any) {
+    const ctx = this;
+    const args = arguments;
+    if (!isThrottle) {
+      func.apply(ctx, args);
+      lastTime = Date.now();
+      isThrottle = true;
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(() => {
+        const nowTime = Date.now();
+        if (nowTime - lastTime >= wait) {
+          func.apply(ctx, args);
+          lastTime = Date.now();
+        }
+      }, Math.max(wait - (Date.now() - lastTime), 0));
+    }
+  };
+}
